2ae361829b8f263f88402a3685b36874b3f71a65
osh <osh8242@gmail.com>
2023-04-12 15:36:58 +0900

    osh getInputValue()

--- Cinema/src/CinemaSystem.java
+++ Cinema/src/CinemaSystem.java
@@ -17,17 +17,21 @@ public class CinemaSystem {
 		
 		outer: while(true) {
 			firstDisplayPrint();
-			getInputvalue();
+			getInputValue();
 			switch(inputValue) {
 				case 1:{//1.회원 로그인
-					//login()
+					//login()					
+					//ReservationProcess reservationProcess = new ReservationProcess();
+					//ReservationProcess.showReservationMenu(isLoggedIn);
 				}
 				case 2:{//2.비회원으로 예매
 					//ReservationProcess reservationProcess = new ReservationProcess();
-					//ReservationProcess.showReservationMenu;
+					//ReservationProcess.showReservationMenu(isLoggedIn);
+					break outer;
 					}
 				case 3:{//3.회원가입
 					//createUser();
+					
 				}
 		
 			
@@ -40,20 +44,33 @@ public class CinemaSystem {
 		
 	}
 	
-	public void getInputvalue() {
+	public void getInputValue() {
 		this.br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			this.inputValue = Integer.parseInt(br.readLine());
 		} catch (Exception e) {
 			System.out.println("숫자를 입력해주세요");
-		}
-		
+		}		
 	}
 	
 	public void firstDisplayPrint() {
 	System.out.println("메뉴 선택 :");
 	System.out.println("[1.회원 로그인]\t[2.비회원으로 예매]\t[3.회원가입]");
 	System.out.println("입력> ");
+	}
+	
+	public void showRevenue() {
+		int revenueSum = 0;
+		for(Reservation r : reservations) {
+			//revenueSum += r.getTheater.getPrice();
+		}
+		System.out.println("총 판매액 : " + revenueSum);
+	}
+	
+	public void showReservations() {
+		for(Reservation r : reservations) {
+			//System.out.println(r.toString());
+		}
 	}
 	
 	
