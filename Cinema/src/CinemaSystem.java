ae2315916bbda3a2ee14ef603c41f08c108dec9f
naeun <nada3377@naver.com>
2023-04-13 11:50:59 +0900

    nana> FileIO main

--- Cinema/src/CinemaSystem.java
+++ Cinema/src/CinemaSystem.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CinemaSystem {
 	
@@ -13,6 +14,54 @@ public class CinemaSystem {
 	private BufferedReader br;	
 	private Member userLoggedIn;
 	
+	
+	// 임시 데이터 셋팅 함수
+	private void setDatasets() {
+		System.out.println("초기 데이터를 셋팅중...");
+		// default 데이터
+		this.theaters.add(new Theater("스즈메", new int[] {3,4}, true));
+		this.theaters.add(new Theater("존윅", new int[] {10,10}, false));
+		this.members.add(new Member(0101234123, "1234", "oshh","오승환"));
+		this.members.add(new Member(0101234567, "5678", "sjsj","장석진"));
+		Theater theater1 = (Theater) this.theaters.get(0);
+		Theater theater2 = (Theater) this.theaters.get(1);
+		User user1 = (User) this.members.get(0);
+		User user2 = (User) this.members.get(1);
+		this.reservations.add(new Reservation( user1, theater1,new int[]{3,4}));
+		this.reservations.add(new Reservation( user2, theater2,new int[]{1,1}));
+        // 임시 데이터 파일로 저장(출력)
+		fileIO.Output(this.theaters, "Theater");
+		fileIO.Output(this.members, "Member");
+		fileIO.Output(this.reservations, "Reservation");		
+	}
+	private void getDatasets() {
+		System.out.println("데이터를 불러옵니다.");
+		// 입력(데이터 불러오기) 
+		this.theaters = fileIO.Input( "Theater");
+		this.members = fileIO.Input( "Member");
+		this.reservations = fileIO.Input( "Reservation");
+		//System.out.println(this.theaters.get(0));
+	}
+	
+	public CinemaSystem() {
+		super();
+
+		this.isLoggedIn = false;
+		this.reservationProcess = new ReservationProcess();
+		
+		this.theaters = new ArrayList<Theater>();
+		this.members = new ArrayList<Member>();
+		this.reservations = new ArrayList<Reservation>();
+		
+		this.fileIO = new FileIO();
+		setDatasets();// 초기데이터 셋팅 : 기능 구현 후 삭제
+		getDatasets();// 입력(데이터 불러오기)
+//		System.out.println(this.theaters.get(0));
+		
+		this.br = br;
+		this.userLoggedIn = userLoggedIn;
+	}
+
 	public void run() {		
 		
 outerMain: while(true) {
--- Cinema/src/FileIO.java
+++ Cinema/src/FileIO.java
@@ -13,8 +13,8 @@ import java.util.List;
 
 public class FileIO {
 		String path = "C:\\Users\\KOSA\\Desktop\\JavaPrj-main\\IOtest\\";
-	//입력 함수
-		public ArrayList<Serializable> Input(String fileName) {
+	// 데이터 불러오기 함수
+		public ArrayList Input(String fileName) {
 //			String fileName =  (loadingDataset instanceof Theater) ? "Theater" :((loadingDataset instanceof Member) ? "Member" :(loadingDataset instanceof Reservation) ? "Reservation" : "noName");
 			
 			String filePath = path+fileName+".txt";
@@ -59,8 +59,8 @@ public class FileIO {
 	   }
 		
 		
-		// 출력 함수
-	   public void Output(List<Serializable> savingDataset, String fileName) {
+		// 데이터 내보내기 함수
+	   public void Output(ArrayList savingDataset, String fileName) {
 		   
 	       FileOutputStream fos = null;
 	       BufferedOutputStream bos = null;
@@ -94,86 +94,5 @@ public class FileIO {
 	   }
 	
 
-	//========================================================================================
 	
-	 /*
-
-	// Theater입력 함수
-	public ArrayList<Theater> Input() {
-		String filename = "C:\\Users\\KOSA\\Desktop\\JavaPrj-main\\IOtest\\theaterList.txt";
-		
-		FileInputStream fis = null;
-		BufferedInputStream bis = null;
-		ObjectInputStream in = null;
-		
-		try {
-			fis = new FileInputStream(filename);
-			bis = new BufferedInputStream(fis);
-			in = new ObjectInputStream(bis);
-			
-			ArrayList<Theater> theaterList = null;
-			theaterList = (ArrayList<Theater>) in.readObject();
-			return theaterList;
-			
-			//원칙적인 처리 
-		}catch (FileNotFoundException e) { //FileNotFoundException
-			System.out.println("파일이 존재 않습니다.");
-		}catch (EOFException e) {
-			System.out.println("끝 " + e.getMessage());
-		}catch (IOException e) {
-			System.out.println("파일을 읽을 수 없습니다.");
-		}catch (ClassNotFoundException e) { //ClassNotFoundException
-			System.out.println("해당 객체가 존재하지 않습니다.");
-		}catch (Exception e) {
-			System.out.println("나머지 예외");
-		}finally {
-			try {
-				in.close();
-				bis.close();
-				fis.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-	      
-	      return null;
-
-   }
-	
-	
-	// 출력 함수
-   public void Output(List<Theater> theaterList) {
-	   
-       FileOutputStream fos = null;
-       BufferedOutputStream bos = null;
-       ObjectOutputStream out=null;
-
-       try {
-    	   
-           fos = new FileOutputStream("C:\\Users\\KOSA\\Desktop\\JavaPrj-main\\IOtest\\theaterList.txt");
-           bos = new BufferedOutputStream(fos);
-           out = new ObjectOutputStream(bos);
-         
-       //직렬화
-           for(int i=0; i<theaterList.size(); i++) {
-        	   out.writeObject(theaterList); 
-           }
-            
-      } catch (Exception e) {
-    	  System.out.println(e.getMessage());
-    	  
-      } finally {
-		  try {
-		     out.close();
-		     bos.close();
-		     fos.close();
-		  } catch (Exception e2) {
-		     e2.printStackTrace();
-		  }
-      }
-      
-   }
-   	  
-	  */
-	 
 }
